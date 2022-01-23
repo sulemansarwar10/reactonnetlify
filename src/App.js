d092fc3187965246b188f1efa646abdf4bd8294d
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-01-23 13:43:04 +0500

    create hello function with netlify
    create hello function by this command
    "netlify functions:create hello"
    add functions folder in toml file.....
    fetch serverless function from app.js
    deploy locally run command"netlify dev"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [mess, setmess] = useState({})
+  useEffect(() => {
+    fetch(`/.netlify/functions/hello?name=from Serverless Function`)
+    .then(response => response.json())
+    .then(data => setmess(data));
+
+
+
+  }, [])
+
+
   return (
     <div className="App">
-   <h1> This is first React app on Netlify by suleman</h1>
+      <h1> This is first React app on Netlify by suleman</h1>
+      <h2>{mess.message}</h2>
     </div>
   );
 }
