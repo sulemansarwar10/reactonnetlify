0bee12f8284c2898c5d1bc24a68d0fefb04ac5ec
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-01-23 12:44:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   <h1> This is first React app on Netlify</h1>
+   <h1> This is first React app on Netlify by suleman</h1>
     </div>
   );
 }
